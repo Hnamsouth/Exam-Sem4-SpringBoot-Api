ab13ea3114c564c6d610c05e2804f0ca6bf27177
Nam Hoang <hnam17397@gmail.com>
2024-04-09 23:27:05 +0700

    test push

---
+++ src/main/java/com/example/examsem4springbootapi/ExamSem4SpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.examsem4springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSem4SpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamSem4SpringBootApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examsem4springbootapi/ExamSem4SpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examsem4springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSem4SpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
